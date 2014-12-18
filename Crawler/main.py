a98a05ab86de948ce263b26721606bd098dd3e2c
Martin Juhasz <info@martinjuhasz.de>
2014-12-18 11:27:19 +0100

    styling

--- Crawler/main.py
+++ Crawler/main.py
@@ -2,25 +2,32 @@ from controller.ArchiveCrawler import ArchiveCrawler
 from controller.ArticleCrawler import ArticleCrawler
 from Lucene.LuceneIndexer import LuceneIndexer
 
+
 def app():
     archive_crawler = ArchiveCrawler()
     article_crawler = ArticleCrawler()
     link_generator = archive_crawler.crawl_post_links()
     lucene_indexer = LuceneIndexer()
 
-
     print
     print "Crawler started. Please wait a moment..."
     print
 
-    for num in range(5):
-        link = link_generator.next()
-        article = article_crawler.crawl(link)
+    for num in range(500):
+        try:
+            link = link_generator.next()
+            article = article_crawler.crawl(link)
 
-        if article:
-            # add article to lucene index
-            lucene_indexer.add_article(article)
-            print article
+            if article:
+                # add article to lucene index
+                lucene_indexer.add_article(article)
+                print article
+        except Exception, e:
+            pass
+
+    print
+    print "Crawler ended. Saving..."
+    print
 
     # commit and write index
     lucene_indexer.write_to_file()
